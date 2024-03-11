53ce9e244e8a00f638c724b3fb09c0e5f694658d
Fred K. Schott <fkschott@gmail.com>
2024-03-10 21:56:17 -0700

    update pre

--- src/pre.ts
+++ src/pre.ts
@@ -10,12 +10,18 @@ const UNIQUE_IDENTIFIER = '<!-- @astrojs/action-studio -->';
 async function run(): Promise<void> {
     const token = core.getInput('github-token')
     const octokit = github.getOctokit(token)
-    const { eventName, repo, payload } = github.context;
+    const { eventName, repo, runId } = github.context;
     console.log(github.context);
     console.log(process.env);
+    console.log({
+      owner: repo.owner,
+      repo: repo.repo,
+      run_id: runId
+    });
 
     // On push to any branch defined in `on: ...`, run `astro db push`
     console.log('Event:', eventName);
+    
     if (eventName !== 'push') {
       return;
     }
@@ -23,7 +29,7 @@ async function run(): Promise<void> {
     const jobInfo = await octokit.rest.actions.listJobsForWorkflowRun({
       owner: repo.owner,
       repo: repo.repo,
-      run_id: payload.runId
+      run_id: runId
     });
     console.log('Job info:', jobInfo);
     const job = jobInfo.data.jobs.find(job => job.name === 'build');
